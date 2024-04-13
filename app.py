51a9a1e77b26bca7ac84c34f64f21cc2fc953d62
Khoa Cao Latitude <87111230+dkhoa2906@users.noreply.github.com>
2024-04-14 02:20:23 +0700

    Initial commit

---
+++ app.py
@@ -0,0 +1,25 @@
+from flask import Flask, request, jsonify
+import pytesseract
+from PIL import Image
+
+app = Flask(__name__)
+
+@app.route('/img2text', methods=['POST'])
+def extract_text():
+    if 'image' not in request.files:
+        return jsonify({'error': 'No image provided'})
+
+    image_file = request.files['image']
+
+    if image_file.filename == '':
+        return jsonify({'error': 'No image filename provided'})
+    # Example of adding any additional options
+
+    image = Image.open(image_file)
+    custom_oem_psm_config = r'--oem 3 --psm 1'
+    custom_lang = 'eng'
+    extracted_text = pytesseract.image_to_string(image, config = custom_oem_psm_config, lang = custom_lang)
+    return jsonify({'message': extracted_text})
+
+if __name__ == '__main__':
+    app.run(debug=True)
